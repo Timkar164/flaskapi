8ee6d5e6ac6227d919cf04794b97d7da0fff2f83
Тимофей Карклин <timkar164@gmail.com>
2021-04-15 19:37:09 +0300

    first commit

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from psycopg2.extras import DictCursor
 from flask import Flask , render_template ,request
 from psycopg2.errors import UndefinedColumn
 from flask_cors import CORS
+from hashlib import sha256
+import time
 
 host = 'ec2-54-164-22-242.compute-1.amazonaws.com'
 port = '5432'
@@ -16,8 +18,55 @@ database = 'de8k09g3q8cu38'
 user = 'froyqwzawvbzre'
 password = '75a90d3b1363ad0f4b13ce32342cd6fe54c5348cd765ad8a5e6e0cdba3bd1cc4'
 rez=''
+test=''
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+class Bot(object):
+ def __init__(self):
+  self.alphabet = ' 1234567890-йцукенгшщзхъфывапролджэячсмитьбюёqwertyuiopasdfghjklzxcvbnm'
+  with open('casein.txt', encoding='utf-8') as f:
+    content = f.read()
+  blocks = content.split('\n')
+  dataset = []
+  for block in blocks:
+    replicas = block.split('\\')[:2]
+    
+    if len(replicas) == 2:
+        pair = [replicas[0], replicas[1]]
+      
+        if pair[0] and pair[1]:
+            dataset.append(pair)
+  X_text = []
+  y = []
+  for question, answer in dataset[:10000]:
+    
+    X_text.append(question)
+    y += [answer]
+  self.vectorizer = CountVectorizer()
+  X = self.vectorizer.fit_transform(X_text)
+  self.clf = LogisticRegression()
+  self.clf.fit(X, y)
+ def clean_str(self,r):
+    r = r.lower()
+    r = [c for c in r if c in self.alphabet]
+    return ''.join(r)
+ def get_generative_replica(self,text):
+    text_vector = self.vectorizer.transform([text])
+    text_vector = text_vector.toarray()[0]
+    question = self.clf.predict([text_vector])
+    question = question[0]
+    print(question)
+    return question
+ def main(self,command):
+    command = command.lower()
+    reply = self.get_generative_replica(command)
+    return reply
 
 
+def makesha(st):
+    st=st+str(int(time.time()))
+    return sha256(st.encode('utf-8')).hexdigest()
 def sql_delet(table_name,param):
     
     conn = psycopg2.connect(dbname=database, user=user, 
@@ -68,7 +117,14 @@ def sql_select(table_name,param):
      cursor.execute('SELECT * FROM '+str(table_name)+' WHERE '+str(param))
     else:
      cursor.execute('SELECT * FROM '+str(table_name))
-    records = cursor.fetchall()
+    records=[]
+    I= True;
+    while I:
+     rez = cursor.fetchone()
+     if rez!=None:
+         records.append(dict(rez))
+     else:
+         I=False
     conn.close()
     cursor.close()
     return {'response':True, 'items':records}
@@ -137,25 +193,205 @@ def api_delet(name,args):
 def api_update(name,args):
     return sql_update(str(name),param_update(dict(args)))
 
+def api_log(log,pas):
+    return sql_select('users',param_select({'log':log,'pas':pas}))
+
+def find_id(pole,znah,ardict):
+    for i in ardict:
+        if int(i[str(pole)])==int(znah):
+            return i
+
 app = Flask(__name__)
 CORS(app)
 
+'''@app.route('/get_messages')
+def indexmes():
+ arg = dict(request.args)
+ param = "( sender = '"+ str(arg['user'])+"' ) OR ( receiver = '"+str(arg['user'])+"' )"
+ param = sql_select('messages',param_select({'id':}))
+ users = sql_select('users','')
+ for i in param['items']:
+    st1=find_id('id',i['maker'],users['items'])
+    st2=find_id('id',i['person'],users['items'])
+    i['maker']=st1['name']
+    i['person']=st2['name']
+    i['img']=st1['img']
+ 
+ return  {'response':True,'items':param['items']}'''
+
+@app.route('/login')
+def log():
+    arg = dict(request.args)
+    log = arg['log']
+    pas = arg['pas']
+    rez = api_log(log,pas)
+    if rez['response'] and rez['items']:
+     return rez
+    else:
+     return {'response':False,'items':'error login'}
+ 
+@app.route('/get_action')
+def indexact():
+ global test
+ arg = dict(request.args)
+ _id=arg['id']
+ action = sql_select('action','')
+ users = []
+ for i in action['items']:
+     users.append(int(i['users']))
+ users=set(users)
+ users=list(users)
+
+ user = sql_select('users',param_select({'id':users}))
+ user=user['items']
+ test=user
+ fr = sql_select('friends',param_select({'users':_id}))
+
+ friend =[]
+ rez=[]
+ for i in action['items']:
+     st=find_id('id',i['users'],user)
+     i['name']=st['name']
+     i['img']=st['img']
+     rez.append(i)
+ 
+ userss = sql_select('users','')
+ userss=userss['items']    
+ for i in fr['items']:
+     try:
+      st=find_id('id',i['friend'],userss)
+      i['name']=st['name']
+      i['img']=st['img']
+      friend.append(i)
+     except:
+         print(i['friend'])
+ 
+    
+     
+ 
+ return  {'response':True,'action': rez,'friend':friend}
+
+
+@app.route('/get_board')
+def indexboard():
+ param = sql_select('board','')
+ users = sql_select('users','')
+ for i in param['items']:
+    st1=find_id('id',i['maker'],users['items'])
+    st2=find_id('id',i['person'],users['items'])
+    i['maker']=st1['name']
+    i['person']=st2['name']
+    i['img']=st1['img']
+ 
+ return  {'response':True,'items':param['items']}
+
+@app.route('/get_idea')
+def indexidea():
+ param = sql_select('idea','')
+ users = sql_select('users','')
+ for i in param['items']:
+    st1=find_id('id',i['maker'],users['items'])
+   
+    i['maker']=st1['name']
+   
+    i['img']=st1['img']
+ 
+ return  {'response':True,'items':param['items']}
+
+@app.route('/get_kurs')
+def indexkurs():
+
+ 
+ param = sql_select('kurses','')
+ param=param['items']
+ 
+ return  {'response': True,'my':param[1:7],'rec':param[8:11],'all':param}
+
+
+
+@app.route('/get_friends')
+def indexfr():
+ arg = dict(request.args)
+ name = arg['user']
+ param = "( sender = '"+ str(name)+"' ) OR ( receiver = '"+str(name)+"' )"
+ return  sql_select('messages',param)
+
+
+@app.route('/get_message')
+def indexm():
+ arg = dict(request.args)
+ name = arg['user']
+ param = "( sender = '"+ str(name)+"' ) OR ( receiver = '"+str(name)+"' )"
+ param=sql_select('messages', param)
+ param=param['items']
+ 
+ users=[]
+ for i in param:
+     users.append(int(i['sender']))
+     users.append(int(i['receiver']))
+ users=set(users)
+ users=list(users)
+ users.remove(int(name))
+ if 200 in users:
+  users.remove(200)
+ rez =[]
+ ur = sql_select('users','')
+ for u in users:
+     stu=find_id('id',u,ur['items'])
+     st={}
+     for j in param:
+         if int(j['sender'])==int(u) or int(j['receiver'])==int(u):
+             st['last']=j['text']
+     st['name']=stu['name']
+     st['img']=stu['img']
+     st['id']=stu['id']
+     rez.append(st)
+    
+     
+     
+     
+ return  {'response':True,'items':rez}
+    
 @app.route('/get_data')
 def index():
- return  api_select('first_table',request.args)
+ arg = dict(request.args)
+ name = arg['table']
+ del(arg['table'])
+ return  api_select(str(name),arg)
 
 @app.route('/set_data')
 def index1():
- return  api_insert('first_table',request.args)
+ arg = dict(request.args)
+ name = arg['table']
+ del(arg['table'])
+ return  api_insert(str(name),arg)
+ 
 
 @app.route('/delet_data')
 def index2():
- return  api_delet('first_table',request.args)
+ arg = dict(request.args)
+ name = arg['table']
+ del(arg['table'])
+ return  api_delet(str(name),arg)
+ 
 
 @app.route('/update_data')
 def index3():
- return  api_update('first_table',request.args)
+ arg = dict(request.args)
+ name = arg['table']
+ del(arg['table'])
+ return  api_update(str(name),arg)
+
+@app.route('/bot')
+def bot():
+
+ arg = dict(request.args)
+ com = arg['command']
+ rez=bot.main(com)
+ return  {'response':True,'bot':rez}
+
 
 
 if __name__=="__main__":
+    bot =Bot()
     app.run(host='0.0.0.0', port=8000)
